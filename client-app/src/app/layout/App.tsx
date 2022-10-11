0ab03702936d45b4a6b1052210099d627938af55
Stephen Lawrance <Stephen.Lawrance@cdsglobal.co.uk>
2022-10-11 16:36:27 +0100

    Up to lecture 54

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { findConfigFile } from "typescript";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard 
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;

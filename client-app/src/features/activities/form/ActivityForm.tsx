395590cffb502332a3d9c5f2a4bc510f3546ad43
Stephen Lawrance <Stephen.Lawrance@cdsglobal.co.uk>
2023-01-31 16:41:33 +0000

    Fix form submit. Complete up to lecture 93.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,13 +10,7 @@ import { useNavigate } from "react-router-dom";
 export default observer(function ActivityForm() {
   const navigate = useNavigate();
   const { activityStore } = useStore();
-  const {
-    createActivity,
-    updateActivity,
-    loading,
-    loadActivity,
-    loadingInitial,
-  } = activityStore;
+  const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
   const { id } = useParams<{ id: string }>();
 
   const [activity, setActivity] = useState({
@@ -34,24 +28,15 @@ export default observer(function ActivityForm() {
   }, [id, loadActivity]);
 
   function handleSubmit() {
-    if (activity.id.length === 0) {
-      let newActivity = {
-        ...activity,
-        id: uuid(),
-      };
-      createActivity(newActivity).then(() =>
-        navigate(`/activities/${newActivity.id}`)
-      );
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
     } else {
-      updateActivity(activity).then(() =>
-        navigate(`/activities/${activity.id}`)
-      );
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
     }
   }
 
-  function handleInputChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
+  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value });
   }
@@ -61,53 +46,14 @@ export default observer(function ActivityForm() {
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete="off">
-        <Form.Input
-          placeholder="Title"
-          value={activity.title}
-          name="title"
-          onChange={handleInputChange}
-        />
-        <Form.TextArea
-          placeholder="Description"
-          value={activity.description}
-          name="description"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="Category"
-          value={activity.category}
-          name="category"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          type="date"
-          placeholder="Date"
-          value={activity.date}
-          name="date"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="City"
-          value={activity.city}
-          name="city"
-          onChange={handleInputChange}
-        />
-        <Form.Input
-          placeholder="Venue"
-          value={activity.venue}
-          name="venue"
-          onChange={handleInputChange}
-        />
-        <Button
-          as={Link}
-          to="/activities"
-          loading={loading}
-          floated="right"
-          positive
-          type="submit"
-          content="Submit"
-        />
-        <Button floated="right" type="button" content="Cancel" />
+        <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
+        <Form.TextArea placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
+        <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
+        <Form.Input type="date" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
+        <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange} />
+        <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange} />
+        <Button loading={loading} floated="right" positive type="submit" content="Submit" />
+        <Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
       </Form>
     </Segment>
   );
